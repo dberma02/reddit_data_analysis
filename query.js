456f9bc536d4c7749b4780c4a9100db0ae637d7c
Daniel Navon <dnavon2557@gmail.com>
2017-12-12 13:17:12 -0500

    Fixed issue with query

--- query.js
+++ query.js
@@ -2,11 +2,144 @@
   var project_id = 'summer-topic-186219';
   var clientId = '364884337709-tfnpb2hpdjojtthmer10glfjf1oqmgft.apps.googleusercontent.com';
   var scopes = 'https://www.googleapis.com/auth/bigquery';
-  var full_query = "SELECT sub_a, sub_b, percent, sub_ac, sub_bc FROM ( SELECT sub_a, sub_b, percent, COUNT(*) OVER(PARTITION BY sub_a) sub_ac, sub_bc FROM ( SELECT a.subreddit sub_a, b.subreddit sub_b, INTEGER(100*COUNT(*) / FIRST(b.author_count) ) percent, COUNT(*) OVER(PARTITION BY sub_b) sub_bc FROM ( SELECT author, subreddit, author_count FROM FLATTEN(( SELECT UNIQUE(author) author, subreddit, COUNT(DISTINCT author) author_count FROM [fh-bigquery:reddit_comments.all] GROUP EACH BY 2 ),author)) a JOIN EACH ( SELECT author, subreddit, author_count FROM FLATTEN(( SELECT UNIQUE(author) author, subreddit, COUNT(DISTINCT author) author_count FROM [fh-bigquery:reddit_comments.all] WHERE subreddit IN ('fatpeoplehate', 'incels', 'pizzagate', 'niggers', 'Coontown', 'hamplanethatred', 'transfags','neofag','shitniggerssay','The_Donald','TheFappening','beatingwomen','jailbait', 'Physical_Removal', 'MensRights', 'findbostonbombers', 'DarkNetMarkets', 'european') GROUP BY 2 ),author) ) b ON a.author=b.author WHERE a.subreddit!=b.subreddit GROUP EACH BY 1, 2 HAVING percent>10 ) ) #WHERE #  sub_ac<20 #  AND sub_bc<20 ORDER BY 2, 3 DESC LIMIT 10";
+  // var full_query = "SELECT sub_a, sub_b, percent, sub_ac, sub_bc FROM ( SELECT sub_a, sub_b, percent, COUNT(*) OVER(PARTITION BY sub_a) sub_ac, sub_bc FROM ( SELECT a.subreddit sub_a, b.subreddit sub_b, INTEGER(100*COUNT(*) / FIRST(b.author_count) ) percent, COUNT(*) OVER(PARTITION BY sub_b) sub_bc FROM ( SELECT author, subreddit, author_count FROM FLATTEN(( SELECT UNIQUE(author) author, subreddit, COUNT(DISTINCT author) author_count FROM [fh-bigquery:reddit_comments.all] GROUP EACH BY 2 ),author)) a JOIN EACH ( SELECT author, subreddit, author_count FROM FLATTEN(( SELECT UNIQUE(author) author, subreddit, COUNT(DISTINCT author) author_count FROM [fh-bigquery:reddit_comments.all] WHERE subreddit IN ('fatpeoplehate', 'incels', 'pizzagate', 'niggers', 'Coontown', 'hamplanethatred', 'transfags','neofag','shitniggerssay','The_Donald','TheFappening','beatingwomen','jailbait', 'Physical_Removal', 'MensRights', 'findbostonbombers', 'DarkNetMarkets', 'european') GROUP BY 2 ),author) ) b ON a.author=b.author WHERE a.subreddit!=b.subreddit GROUP EACH BY 1, 2 HAVING percent>10 ) ) #WHERE #  sub_ac<20 #  AND sub_bc<20 ORDER BY 2, 3 DESC LIMIT 10";
+  /*
+  var full_query = `SELECT
+  sub_a,
+  sub_b,
+  percent,
+  sub_ac,
+  sub_bc,
+  _rank
+FROM (
+  SELECT
+    sub_a,
+    sub_b,
+    percent,
+    COUNT(*) OVER(PARTITION BY sub_a) sub_ac,
+    sub_bc,
+    DENSE_RANK() OVER(PARTITION BY sub_B ORDER BY percent DESC) _rank
+  FROM (
+    SELECT
+      a.subreddit sub_a,
+      b.subreddit sub_b,
+      INTEGER(100*COUNT(*) / FIRST(b.author_count) ) percent,
+      COUNT(*) OVER(PARTITION BY sub_b) sub_bc,
+    FROM (
+      SELECT
+        author,
+        subreddit, author_count
+      FROM
+        FLATTEN((
+          SELECT
+            UNIQUE(author) author, 
+            subreddit, COUNT(DISTINCT author) author_count
+          FROM
+            [fh-bigquery:reddit_comments.all]
+            WHERE author != "[deleted]"
+          GROUP EACH BY
+            2 ),author)) a
+    JOIN EACH (
+      SELECT
+        author,
+        subreddit, author_count
+      FROM
+        FLATTEN((
+          SELECT
+            UNIQUE(author) author,
+            subreddit, COUNT(DISTINCT author) author_count
+          FROM
+            [fh-bigquery:reddit_comments.all]
+          WHERE
+            subreddit IN ('fatpeoplehate', 'incels', 'pizzagate', 'niggers', 'Coontown', 'hamplanethatred', 'transfags', 'neofag','shitniggerssay', 'The_Donald', 'TheFappening', 'beatingwomen', 'Creepshots', 'jailbait', 'Physical_Removal', 'MensRights', 'findbostonbombers', 'DarkNetMarkets', 'european')
+            AND author != "[deleted]"
+          GROUP BY
+            2 ),author) ) b
+    ON
+      a.author=b.author
+    WHERE
+      a.subreddit!=b.subreddit
+    GROUP EACH BY
+      1,
+      2
+    HAVING
+      percent>10 ) )
+HAVING
+  _rank <= 10
+ORDER BY
+  2,
+  3 DESC`;
   var config = {
     'client_id': clientId,
     'scope': scopes
-  };
+  };*/
+  var full_query = `SELECT
+  sub_a,
+  sub_b,
+  percent,
+  sub_ac,
+  sub_bc,
+  _rank
+FROM (
+  SELECT
+    sub_a,
+    sub_b,
+    percent,
+    COUNT(*) OVER(PARTITION BY sub_a) sub_ac,
+    sub_bc,
+    DENSE_RANK() OVER(PARTITION BY sub_B ORDER BY percent DESC) _rank
+  FROM (
+    SELECT
+      a.subreddit sub_a,
+      b.subreddit sub_b,
+      INTEGER(100*COUNT(*) / FIRST(b.author_count) ) percent,
+      COUNT(*) OVER(PARTITION BY sub_b) sub_bc,
+    FROM (
+      SELECT
+        author,
+        subreddit, author_count
+      FROM
+        FLATTEN((
+          SELECT
+            UNIQUE(author) author, 
+            subreddit, COUNT(DISTINCT author) author_count
+          FROM
+            [fh-bigquery:reddit_comments.all]
+            WHERE author NOT IN (SELECT author FROM [fh-bigquery:reddit_comments.bots_201505])
+                        AND author != "[deleted]"
+          GROUP EACH BY
+            2 ),author)) a
+    JOIN EACH (
+      SELECT
+        author,
+        subreddit, author_count
+      FROM
+        FLATTEN((
+          SELECT
+            UNIQUE(author) author,
+            subreddit, COUNT(DISTINCT author) author_count
+          FROM
+            [fh-bigquery:reddit_comments.all]
+          WHERE
+            subreddit IN ('fatpeoplehate', 'incels', 'pizzagate', 'niggers', 'Coontown', 'hamplanethatred', 'transfags', 'neofag','shitniggerssay', 'The_Donald', 'TheFappening', 'beatingwomen', 'Creepshots', 'jailbait', 'Physical_Removal', 'MensRights', 'findbostonbombers', 'DarkNetMarkets', 'european')
+            AND author NOT IN (SELECT author FROM [fh-bigquery:reddit_comments.bots_201505])
+            AND author != "[deleted]"
+          GROUP BY
+            2 ),author) ) b
+    ON
+      a.author=b.author
+    WHERE
+      a.subreddit!=b.subreddit
+    GROUP EACH BY
+      1,
+      2
+    HAVING
+      percent>10 ) )
+HAVING
+  _rank <= 20
+ORDER BY
+  2,
+  3 DESC`;
 
   function runQuery() {
    var data;
